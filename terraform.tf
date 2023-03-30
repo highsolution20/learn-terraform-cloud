45d5588e651b07640fd5b72ffa426e96e0fb1e80
HtechCloud <htechglobalinvest@gmail.com>
2023-03-29 22:04:58 -0400

    updated

--- terraform.tf
+++ terraform.tf
@@ -8,12 +8,13 @@
     }
   }
 */
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.28.0"
-    }
+required_providers {
+  aws = {
+    source  = "hashicorp/aws"
+    version = "~> 3.28.0"
   }
+}
 
-  required_version = ">= 0.14.0"
+required_version {
+  version = ">= 0.14.0"
 }
